5cad3e2ccbbb962f7f02eb0360f81bedf2d8027a
David Tolman <dajoto1@gmail.com>
2022-03-11 13:40:23 +0000

    add docs from build

---
+++ docs/precache-manifest.6571deed54b53a97ba2aa82714caa469.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9675ea558cec89dbe3021167d58307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79fe028e321b3b748b0d",
+    "url": "/static/css/main.bea54f84.chunk.css"
+  },
+  {
+    "revision": "18d0012ff1e93c36e87c",
+    "url": "/static/js/2.fac3a2c1.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/static/js/2.fac3a2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fe028e321b3b748b0d",
+    "url": "/static/js/main.b7fc717d.chunk.js"
+  },
+  {
+    "revision": "6ce07bb538fd07ff8ae5",
+    "url": "/static/js/runtime-main.6c8890a7.js"
+  },
+  {
+    "revision": "3ada9815c06d2619d52d16a5601c96b2",
+    "url": "/static/media/Early-GameBoy.3ada9815.ttf"
+  }
+]);
